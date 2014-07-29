848af166dfe56607bfb35ecd912efa39fc80b0c2
Tom Aldcroft <taldcroft@gmail.com>
2014-07-28 22:26:03 -0400

    Add docs and add control over exceptions

--- ska_path/__init__.py
+++ ska_path/__init__.py
@@ -1,3 +1,6 @@
+"""
+Utility function to find file paths in the Ska environment.
+"""
 import os
 import sys
 
@@ -8,15 +11,53 @@ def root_path():
     return os.path.abspath(os.sep)
 
 
-def ska_path():
+def ska_path(*path_args, **kwargs):
+    """
+    Return the path corresponding to supplied ``args`` in the Ska
+    environment root.
+
+    The first option of the following which exists is returned:
+
+    - SKA environment variable / *path_args
+    - /proj/sot/ska / *path_args
+    - HOME_DIRECTORY / ska / *path_args
+    - . / *path_args
+
+    If none exist then None is returned if ``raise`` is False (default),
+    otherwise an IOError exception is raised.
+
+    Examples::
+
+      >>> from ska_path import ska_path
+      >>> AGASC_DATA = ska_path('data', 'agasc')
+      >>> ska_path()
+      '/proj/sot/ska'
+      >>> ska_path('data', 'blah', require_match=True)
+      IOError: Could not find data/blah in any valid Ska path
+
+    :param *path_args: zero, one or more path strings
+    :param require_match: bool, if True then require a matching path
+    :returns: path or None (if require_match=False)
+    """
+    paths = []
+
     ska = os.environ.get('SKA')
     if ska is not None:
-        return ska
+        paths.append(os.path.join(ska, *path_args))
+
     homevar = 'HOMEPATH' if sys.platform.startswith('win') else 'HOME'
     root = root_path()
-    for path in (os.path.join(root, 'proj', 'sot', 'ska'),
-                 os.path.join(os.environ.get(homevar, root), 'ska')):
+
+    paths.append(os.path.join(root, 'proj', 'sot', 'ska', *path_args))
+    paths.append(os.path.join(os.environ.get(homevar, root), 'ska', *path_args))
+    paths.append(os.path.join('', *path_args))
+
+    for path in paths:
         if os.path.exists(path):
             return path
 
-    raise IOError('No Ska root directory found')
+    if kwargs.get('require_match'):
+        raise IOError('Could not find {} in any valid Ska path'
+                      .format(os.path.join(*path_args)))
+    else:
+        return None
